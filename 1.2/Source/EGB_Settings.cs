d377b7bbc69171f65304982fb5b43610e8dd712f
Squirting Elephant <squirtingelephant@gmail.com>
2021-07-20 22:34:21 +0200

    Update for version 1.3
    The change is only a single xml line but I separated the versions into their own folders (1.2 and 1.3). It looks like a huge change but it isn't.

---
+++ 1.2/Source/EGB_Settings.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace EndgameBuildings
+{
+    public class EGB_Settings : Mod
+    {
+        public static EGB_SettingsData Settings;
+        private const float MAX_FIREFOAM_POPPER_RADIUS = 56.0f;
+
+        /// <summary>
+        /// A mandatory constructor.
+        /// </summary>
+        public EGB_Settings(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<EGB_SettingsData>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+            ls.Begin(inRect);
+
+            string bufferResCost = Settings.HT_ResearchCost.ToString();
+            ls.TextFieldNumericLabeled<int>(Constants.Localizations.RESEARCH_COST.Translate(), ref Settings.HT_ResearchCost, ref bufferResCost, 0, 100000);
+
+            string bufferReactorOutput = Settings.HT_ReactorOutput.ToString();
+            ls.TextFieldNumericLabeled<int>(Constants.Localizations.REACTOR_OUTPUT.Translate(), ref Settings.HT_ReactorOutput, ref bufferReactorOutput, 1, 1000000);
+
+            string bufferPopper = Settings.HT_FireFoamPopperRadius.ToString();
+            ls.TextFieldNumericLabeled<float>(Constants.Localizations.FIREFOAM_POPPER_RADIUS.Translate() + Settings.HT_FireFoamPopperRadius.ToString(), ref Settings.HT_FireFoamPopperRadius, ref bufferPopper, 1.0f, MAX_FIREFOAM_POPPER_RADIUS);
+            Settings.HT_FireFoamPopperRadius = ls.Slider(Settings.HT_FireFoamPopperRadius, 1.0f, MAX_FIREFOAM_POPPER_RADIUS); // Max allowed by game: 56.6f.
+
+            EGB_OnDefsLoaded.ApplySettingsToDefs();
+
+            ls.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        /// <summary>
+        /// This is the name as how it'll appear in the game's settings menu.
+        /// </summary>
+        public override string SettingsCategory() => Constants.Localizations.SETTINGS_CATEGORY.Translate();
+    }
+}
---
+++ 1.3/Source/EGB_OnDefsLoaded.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System;
+using System.Linq;
+using Verse;
+
+namespace EndgameBuildings
+{
+    [StaticConstructorOnStartup] // This attribute calls the static constructor AFTER the defs are loaded.
+    public class EGB_OnDefsLoaded
+    {
+        static EGB_OnDefsLoaded()
+        {
+            ApplySettingsToDefs();
+        }
+
+        public static void ApplySettingsToDefs()
+        {
+            DefDatabase<ThingDef>.GetNamed(Constants.DefNames.FUSION_REACTOR).comps.OfType<CompProperties_Power>().First().basePowerConsumption = -Math.Abs(EGB_Settings.Settings.HT_ReactorOutput);
+
+            ThingDef ht_FireFoamPopper = DefDatabase<ThingDef>.GetNamed(Constants.DefNames.FIREFOAM_POPPER_MKII);
+            ht_FireFoamPopper.comps.OfType<CompProperties_Explosive>().First().explosiveRadius = EGB_Settings.Settings.HT_FireFoamPopperRadius;
+            ht_FireFoamPopper.specialDisplayRadius = EGB_Settings.Settings.HT_FireFoamPopperRadius;
+        }
+    }
+}
